8b7d191d691e00f6a7babd694f935d1d8c1019c7
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-05-31 16:29:16 -0300

    Comecando a implementar os movimentos da torre

--- xadrez-console/Xadrez/Rei.cs
+++ xadrez-console/Xadrez/Rei.cs
@@ -6,6 +6,10 @@ namespace Xadrez {
         : base(tab, cor){
         }
 
+        private bool PodeMover(Posicao pos) {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
         public override bool[,] MovimentosPossiveis() {
             bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
             Posicao pos = new Posicao(0, 0);
@@ -42,10 +46,6 @@ namespace Xadrez {
                 mat[pos.Linha, pos.Coluna] = true;
             }
             return mat;
-        }
-        private bool PodeMover(Posicao pos) {
-            Peca p = Tab.Peca(pos);
-            return p == null || p.Cor != Cor;
         }
         public override string ToString() {
             return "R";

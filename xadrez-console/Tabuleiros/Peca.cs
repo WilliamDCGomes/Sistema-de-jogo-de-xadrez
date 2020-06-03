51cf21816fd36243cb76e66312e3c10e66c89972
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-06-03 20:50:44 -0300

    Correcao: mudando o nome do metodo para MovimentoPossivel

--- xadrez-console/Tabuleiros/Peca.cs
+++ xadrez-console/Tabuleiros/Peca.cs
@@ -29,7 +29,7 @@
             }
             return false;
         }
-        public bool PodeMoverPara(Posicao pos) {
+        public bool MovimentoPossivel(Posicao pos) {
             return MovimentosPossiveis()[pos.Linha, pos.Coluna];
         }
         public abstract bool[,] MovimentosPossiveis();
